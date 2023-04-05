e7220da7ac5a92489672480edbea628ec5c6d4bd
Johnny Harbieh <jharbieh@microsoft.com>
2023-04-05 10:08:35 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # README
+
+## Options to acquire Azure DevOps Work Item data
+1. Azure DevOps REST API
+2. Azure DevOps Python SDK
+3. Azure DevOps CLI
+4. Azure DevOps PowerShell
+5. Azure DevOps Node.js SDK
+6. Azure DevOps OData REST API
+
+## How to run the script
+```
+python main.py
+```
+
+## How to install the azure devops python sdk
+[Azure DevOps Python SDK](https://docs.microsoft.com/en-us/azure/devops/integrate/quickstarts/client-library?view=azure-devops&tabs=python)
+
+## Run VSCode as Administrator
+```
+python -m ensurepip
+python -m pip install --upgrade pip
+python -m pip install azure-devops
+python -m pip install --upgrade azure-devops
+python -m pip install requests
+python -m pip install dotenv
+```
+
+## Resources
+[Azure DevOps Work Item REST API](https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items?view=azure-devops-rest-7.0)
+
+[Azure DevOps Work Item OData REST API](https://learn.microsoft.com/en-us/azure/devops/report/powerbi/odataquery-connect?view=azure-devops)
+
+[Azure DevOps Analytics Quick Reference](https://learn.microsoft.com/en-us/azure/devops/report/extend-analytics/quick-ref?view=azure-devops)
--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# Options to acquire Azure DevOps Work Item data
-# 1. Azure DevOps REST API
-# 2. Azure DevOps Python SDK
-# 3. Azure DevOps CLI
-# 4. Azure DevOps PowerShell
-# 5. Azure DevOps Node.js SDK
-# 6. Azure DevOps OData REST API
-
-# Azure DevOps Work Item REST API
-# https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items?view=azure-devops-rest-7.0
-
-# Azure DevOps Work Item OData REST API
-# https://learn.microsoft.com/en-us/azure/devops/report/powerbi/odataquery-connect?view=azure-devops
-
-# Azure DevOps Analytics Quick Reference
-# https://learn.microsoft.com/en-us/azure/devops/report/extend-analytics/quick-ref?view=azure-devops
-
 import requests
 import json
 import os
@@ -131,17 +114,3 @@ def query_azure_devops_org_project_workitems(type=work_item_type, state=work_ite
 
 if __name__ == "__main__":
     query_azure_devops_org_project_workitems()
-
-# How to run the script
-# python main.py
-
-# How to install the azure devops python sdk
-# https://docs.microsoft.com/en-us/azure/devops/integrate/quickstarts/client-library?view=azure-devops&tabs=python
-
-# Run VSCode as Administrator
-# python -m ensurepip
-# python -m pip install --upgrade pip
-# python -m pip install azure-devops
-# python -m pip install --upgrade azure-devops
-# python -m pip install requests
-# python -m pip install dotenv
